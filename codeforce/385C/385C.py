44703515f7c0f831f137b9defdee1ca36654d13f
Zhiwen Xie <TiwazBiu@gmail.com>
2020-02-06 00:05:33 +0800

    385C with memorization

--- codeforce/385C/385C.py
+++ codeforce/385C/385C.py
@@ -4,38 +4,38 @@ def list_input():
 def int_input():
     return int(input())
 
+prime_number = {}
 def find_primes(l, r):
     def is_prime(x):
         from math import sqrt
         if x < 2:
             return False
-        return all([x % k for k in range(2, int(sqrt(x)) + 1)])
+        elif x in prime_number:
+            return prime_number[x]
+        else:
+            prime_number[x] = all([x % k for k in range(2, int(sqrt(x)) + 1)])
+            return prime_number[x]
     lst = []
     for i in range(l, r+1):
         if is_prime(i):
             lst.append(i)
     return lst
 
-def ans(lst, primes):
-    total = 0
-    for num in lst:
-        for p in primes:
-            if num % p == 0:
-                total += 1
-    return total
-
 def main():
+    answers = {}
+    def solve(primes):
+        total = 0
+        for p in primes:
+            if p not in answers:
+                answers[p] = sum([x % p == 0 for x in lst])
+            total += answers[p]
+        return total
     n = int_input()
     lst = list_input()
     m = int_input()
-    # print(n)
-    # print(lst)
-    # print(m)
     for _ in range(m):
         l, r = list_input()
-        # print(l, r)
         primes = find_primes(l, r)
-        # print("primes: {}".format(primes))
-        print(ans(lst, primes))
+        print(solve(primes))
 
 main()
