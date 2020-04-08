861b68d3a1ee14a37b4f1fcd9c47e436ffe517f6
Zhiwen Xie <TiwazBiu@gmail.com>
2020-04-08 16:51:27 +0800

    [leetcode]improved 1.TwoSum hashmap solution

--- leetcode/1/hash.py
+++ leetcode/1/hash.py
@@ -1,17 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        m = {}
-        for i, num in enumerate(nums):
-            if num not in m:
-                m[num] = [i]
+        d = {}
+        for i, x in enumerate(nums):
+            val = target - x
+            if val in d:
+                return d[val], i
             else:
-                m[num].append(i)
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in m:
-                lst = m[complement]
-                for j in lst:
-                    if j == i:
-                        continue
-                    else:
-                        return [i, j]
+                d[x] = i
