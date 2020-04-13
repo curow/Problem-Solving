a8cb10d26f984e393c00300e0ccab5c141ed64c1
Zhiwen Xie <TiwazBiu@gmail.com>
2020-04-13 21:15:23 +0800

    [leetcode 30 day challenge]solved week 2 6th problem: Contiguous Array by implementing the idea presented in official editorial

--- leetcode/30-day-challenge/2.6/clear.py
+++ leetcode/30-day-challenge/2.6/clear.py
@@ -1,19 +1,16 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
+        balances = {}
+        count = 0
+        balances[count] = -1
+        best = 0
         for i, num in enumerate(nums):
             if num == 0:
-                nums[i] = -1
-        size = len(nums)
-        dp = [[float('inf') for j in range(size)] for i in range(size)]
-        best = 0
-        for i in range(size):
-            dp[i][0] = nums[i]
-        for i in range(1, size):
-            for j in range(1, i + 1):
-                dp[i][j] = dp[i][0] + dp[i - 1][j - 1]
-                if dp[i][j] == 0:
-                    best = max(best, j + 1)
-        for j in range(size - 1, -1, -1):
-            if dp[size - 1][j] == 0:
-                return j + 1
-        return 0
+                count -= 1
+            else:
+                count += 1
+            if count not in balances:
+                balances[count] = i
+            else:
+                best = max(best, i - balances[count])
+        return best
