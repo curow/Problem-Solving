cee62f2410d95ba1279978c5f1675e1157c42cfe
Zhiwen Xie <TiwazBiu@gmail.com>
2020-04-16 19:19:35 +0800

    [leetcode 30 day challenge]sovled week 3 2st problem: Valid Parenthesis String using dynamic programming with memorization

---
+++ leetcode/30-day-challenge/3.2/ValidParenthesisString.py
@@ -0,0 +1,29 @@
+from functools import lru_cache
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        @lru_cache(maxsize=None)
+        def search(k, debt):
+            if k >= len(s):
+                return debt == 0
+
+            if s[k] == '(':
+                debt += 1
+                return search(k + 1, debt)
+            elif s[k] == ')':
+                if debt > 0:
+                    debt -= 1
+                    return search(k + 1, debt)
+                else:
+                    return False
+            elif s[k] == '*':
+                if search(k + 1, debt):
+                    return True
+                if search(k + 1, debt + 1):
+                    return True
+                if debt > 0 and search(k + 1, debt - 1):
+                        return True
+                return False
+        return search(0, 0)
+
+
+
