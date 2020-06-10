9aa0d89941d13c2b61162358b9ba5ae3c6ebad08
Zhiwen Xie <TiwazBiu@gmail.com>
2020-06-10 20:32:28 +0800

    LeetCode 21. Merge Two Sorted Lists simplified

--- leetcode/21. Merge Two Sorted Lists/solution.cc
+++ leetcode/21. Merge Two Sorted Lists/solution.cc
@@ -13,15 +13,19 @@ public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         ListNode *dummy = new ListNode(-1);
         ListNode *p = dummy;
+
         while (l1 && l2) {
-            if (l1->val <= l2->val) p->next = l1, l1 = l1->next;
-            else p->next = l2, l2 = l2->next;
+            if (l1->val <= l2->val) {
+                p->next = l1;
+                l1 = l1->next;
+            } else {
+                p->next = l2;
+                l2 = l2->next;
+            }
             p = p->next;
         }
-        if (l1) p->next = l1;
-        else p->next = l2;
-        p = dummy->next;
-        delete dummy;
-        return p;
+        p->next = l1 ? l1 : l2;
+
+        return dummy->next;
     }
 };
