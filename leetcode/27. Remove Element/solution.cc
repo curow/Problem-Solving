9423257c793c3938b69b6371e6af1d1e3ab354e9
Zhiwen Xie <TiwazBiu@gmail.com>
2020-06-12 20:51:36 +0800

    LeetCode 27. Remove Element second solution

--- leetcode/27. Remove Element/solution.cc
+++ leetcode/27. Remove Element/solution.cc
@@ -2,14 +2,11 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int n = size(nums);
-        int i = 0, j = 0;
-        while (j < n) {
-            while (j < n && nums[j] == val) ++j;
-            if (j < n) {
-                nums[i] = nums[j];
-                ++i;
-                ++j;
-            }
+        int i = 0, j = n - 1;
+        while (i < j) {
+            while (i < n && nums[i] != val) ++i;
+            while (j >= 0 && nums[j] == val) --j;
+            if (i < j) swap(nums[i], nums[j]);
         }
         return i;
     }
