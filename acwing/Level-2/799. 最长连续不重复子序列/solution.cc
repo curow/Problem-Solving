e3932d209b4be9033fc68323433d556f77b5c17d
Zhiwen Xie <TiwazBiu@gmail.com>
2020-06-14 19:20:57 +0800

    AcWing 799. 最长连续不重复子序列 final

--- acwing/Level-2/799. 最长连续不重复子序列/solution.cc
+++ acwing/Level-2/799. 最长连续不重复子序列/solution.cc
@@ -9,14 +9,13 @@ int main() {
     cin >> n;
     vector<int> a(n, 0);
     for (int i = 0; i < n; ++i) cin >> a[i];
-    unordered_map<int, int> count;
     int res = 0;
+    unordered_map<int, int> loc;
     for (int i = 0, j = 0; j < n; ++j) {
-        ++count[a[j]];
-        while (i < j && count[a[j]] > 1) {
-            --count[a[i]];
-            ++i;
+        if (loc.count(a[j]) && loc[a[j]] >= i) {
+            i = loc[a[j]] + 1;
         }
+        loc[a[j]] = j;
         res = max(res, j - i + 1);
     }
     cout << res << endl;
