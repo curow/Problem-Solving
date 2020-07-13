cc6060b7027264db159afb644a57e7ab7f0122b9
Zhiwen Xie <TiwazBiu@gmail.com>
2020-07-13 11:40:36 +0800

    [kickstart 2020 round D]practice A with cpp

--- kickstart/2020/D/A/practice.cc
+++ kickstart/2020/D/A/practice.cc
@@ -1,12 +1,31 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
+
 int main() {
-	int t;
-	cin >> t;
-	for (int i = 1; i <= t; ++i) {
-        int n, m;
-		cin >> n >> m;
-		cout << "Case #" << i << ": " << endl;
+	int T;
+	cin >> T;
+	for (int t = 1; t <= T; ++t) {
+        int n;
+		cin >> n;
+
+        vector<int> v(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> v[i];
+        }
+
+        int breaking_num = -1, cnt = 0;
+        for (int i = 0; i < n; ++i) {
+            if (v[i] > breaking_num) {
+                breaking_num = v[i];
+                if (i == n - 1 || v[i] > v[i + 1]) {
+                    ++cnt;
+                }
+            }
+        }
+
+		cout << "Case #" << t << ": " << cnt << endl;
 	}
 	return 0;
 }
