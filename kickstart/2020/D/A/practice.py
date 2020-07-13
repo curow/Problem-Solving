235c0dce6676b0b00b79e29625d651b2fa470503
Zhiwen Xie <TiwazBiu@gmail.com>
2020-07-13 11:15:41 +0800

    [kickstart 2020 round D]corrected python template file

--- kickstart/2020/D/A/practice.py
+++ kickstart/2020/D/A/practice.py
@@ -1,4 +1,4 @@
 T = int(input())
 for t in range(1, T + 1):
     n, m = [int(s) for s in input().split(" ")]
-    print("Case #{}: {}".format(t, n))
+    print("Case #{}: {} {}".format(i, n + m, n * m))
