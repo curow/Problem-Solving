3d729f933ba08e57faf5d7becdecb3362ab2c976
Zhiwen Xie <TiwazBiu@gmail.com>
2020-07-14 18:57:41 +0800

    [acwing level-2]dp problem complete knapsack (TLE, but simplified the code)

--- acwing/Level-2/3. 完全背包问题/solution.cc
+++ acwing/Level-2/3. 完全背包问题/solution.cc
@@ -16,11 +16,8 @@ int main() {
     vector<vector<int>> dp(n + 1, vector<int>(c + 1, 0));
     for (int i = 1; i <= n; ++i) {
         for (int j = 1; j <= c; ++j) {
-            int k = 1;
-            dp[i][j] = dp[i - 1][j];
-            while (j >= k * w[i]) {
+            for (int k = 0; k * w[i] <= j; ++k) {
                 dp[i][j] = max(dp[i][j], dp[i - 1][j - k * w[i]] + k * v[i]);
-                k += 1;
             }
         }
     }
