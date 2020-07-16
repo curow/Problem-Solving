0476a96cab2a6d23e17a5381596947a316de403e
Zhiwen Xie <TiwazBiu@gmail.com>
2020-07-16 19:14:55 +0800

    [acwing level-2]数字三角形，由底向上求解

--- acwing/Level-2/898. 数字三角形/solution.cc
+++ acwing/Level-2/898. 数字三角形/solution.cc
@@ -8,28 +8,25 @@ int main() {
     int n;
     cin >> n;
     vector<vector<int>> v(n);
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < i + 1; ++j) {
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 0; j < i; ++j) {
             int x;
             cin >> x;
-            v[i].push_back(x);
+            v[n - i].push_back(x);
         }
     }
 
-    vector<int> f(n, 0);
-    f[0] = v[0][0];
+    vector<int> dp(n);
+    for (int i = 0; i < n; ++i) {
+        dp[i] = v[0][i];
+    }
+
     for (int i = 1; i < n; ++i) {
-        for (int j = i; j >= 0; --j) {
-            if (j == i) {
-                f[j] = v[i][j] + f[j - 1];
-            } else if (j == 0) {
-                f[j] = v[i][j] + f[j];
-            } else {
-                f[j] = v[i][j] + max(f[j], f[j - 1]);
-            }
+        for (int j = 0; j < n - i; ++j) {
+            dp[j] = v[i][j] + max(dp[j], dp[j + 1]);
         }
     }
 
-    cout << *max_element(f.begin(), f.end()) << endl;
+    cout << dp[0] << endl;
     return 0;
 }
