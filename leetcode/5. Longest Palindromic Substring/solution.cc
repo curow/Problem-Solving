475b3fa738fce79165bd473eb735df45b6460baf
Zhiwen Xie <TiwazBiu@gmail.com>
2020-07-21 21:50:11 +0800

    最长回文子串，DP解法

---
+++ leetcode/5. Longest Palindromic Substring/solution.cc
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.size() <= 1) return s;
+        const int n = s.size();
+        string best = s.substr(0, 1);
+        vector<vector<bool>> f(n, vector<bool>(n, false));
+        for (int i = 0; i < n; ++i) f[i][i] = true;
+        for (int len = 2; len <= n; ++len) {
+            for (int i = 0; i + len - 1 < n; ++i) {
+                int j = i + len - 1;
+                if (s[i] == s[j] && (i + 1 >= j - 1 || f[i + 1][j - 1])) {
+                    f[i][j] = true;
+                    if (len > best.size()) {
+                        best = s.substr(i, len);
+                    }
+                }
+            }
+        }
+        return best;
+    }
+};
