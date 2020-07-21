5d5ea1a0c578e378e2513f79758e7da73978600d
Zhiwen Xie <TiwazBiu@gmail.com>
2020-07-22 00:08:45 +0800

    DP求解爬楼梯, 用tie对两个变量同时赋值

--- leetcode/70. Climbing Stairs/solution.cc
+++ leetcode/70. Climbing Stairs/solution.cc
@@ -3,9 +3,7 @@ public:
     int climbStairs(int n) {
         int a = 1, b = 1;
         for (int i = 0; i < n - 1; ++i) {
-            int x = b;
-            b += a;
-            a = x;
+            tie(a, b) = tuple<int, int>{b, a + b};
         }
         return b;
     }
