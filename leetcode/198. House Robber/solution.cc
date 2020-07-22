9eb97409bf4fa1a28b90a66e5fb879349df80f3a
Zhiwen Xie <TiwazBiu@gmail.com>
2020-07-22 10:20:01 +0800

    DP 抢劫

---
+++ leetcode/198. House Robber/solution.cc
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        const int n = nums.size();
+        if (n == 0) return 0;
+        vector<int> f(n);
+        for (int i = 0; i < n; ++i) {
+            f[i] = max(nums[i] + (i >= 2 ? f[i - 2] : 0), i >= 1 ? f[i - 1] : 0);
+        }
+        return f[n - 1];
+    }
+};
