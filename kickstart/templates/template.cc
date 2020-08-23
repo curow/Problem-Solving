58da07b499cfd5f20b2c8844708eb89e6c95ca3b
Zhiwen Xie <TiwazBiu@gmail.com>
2020-08-23 10:38:43 +0800

    changed kickstart c++ template

--- kickstart/templates/template.cc
+++ kickstart/templates/template.cc
@@ -1,8 +1,15 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
+#define int __int64_t
 using namespace std;
 
-int main() {
+__int32_t main() {
+    #ifdef TIMING 
+    auto start = chrono::steady_clock::now();
+    #endif
+    cin.sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // solve the problem
 	int T;
 	cin >> T;
 	for (int t = 1; t <= T; ++t) {
@@ -11,5 +18,12 @@ int main() {
 
 		cout << "Case #" << t << ": " << endl;
 	}
-	return 0;
+
+    #ifdef TIMING 
+    auto end = chrono::steady_clock::now();
+    cout << "\nElapsed time in milliseconds : "
+         << chrono::duration_cast<chrono::milliseconds>(end - start).count()
+         << " ms\n";
+    #endif
+    return 0;
 }
