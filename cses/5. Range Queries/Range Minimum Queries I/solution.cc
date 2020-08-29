c246f30cec15b39bd1be2c92287a176cdc0bceac
Zhiwen Xie <TiwazBiu@gmail.com>
2020-08-30 00:23:00 +0800

    Range Minimum Queries I, implemented as cp-algorithms describe

--- cses/5. Range Queries/Range Minimum Queries I/solution.cc
+++ cses/5. Range Queries/Range Minimum Queries I/solution.cc
@@ -1,41 +1,41 @@
 #include <bits/stdc++.h>
+#define int __int64_t
 using namespace std;
- 
-const int N = 5e7;
-struct pair_hash {
-    size_t operator () (const pair<int, int> &p) const {
-        int x = ((p.second - p.first + 1) >> 16) + p.second;
-        return hash<int>{}(x);
-    }
-};
-unordered_map<pair<int, int>, int, pair_hash> f(N);
 
-int main() {
+const int N = 2e5 + 10, K = 18;
+int f[N][K + 1], log_table[N];
+
+
+__int32_t main() {
     #ifdef TIMING 
     auto start = chrono::steady_clock::now();
     #endif
- 
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     // solve the problem
-    int n, q;
-    scanf("%d %d", &n, &q);
-    for (int i = 1; i <= n; ++i) {
-        int x;
-        scanf("%d", &x);
-        f[{i, i}] = x;
+    log_table[1] = 0;
+    for (int i = 2; i < N; ++i) {
+        log_table[i] = log_table[i / 2] + 1;
     }
-    for (int k = 2; k <= n; k *= 2) {
-        for (int i = 1; i + k - 1 <= n; ++i) {
-            int j = i + k - 1;
-            f[{i, j}] = min(f[{i, i + k / 2 - 1}], f[{i + k / 2, j}]);
+    int n, q;
+    cin >> n >> q;
+    for (int i = 0; i < n; ++i) {
+        cin >> f[i][0];
+    }
+    for (int j = 1; (1 << j) <= n; ++j) {
+        for (int i = 0; i + (1 << j) <= n; ++i) {
+            f[i][j] = min(f[i][j - 1], f[i + (1 << (j - 1))][j - 1]);
         }
     }
     for (int i = 0; i < q; ++i) {
         int a, b;
-        scanf("%d %d", &a, &b);
-        int w = (int) pow(2, (int) log2(b - a + 1));
-        printf("%d\n", min(f[{a, a + w - 1}], f[{b - w + 1, b}]));
+        cin >> a >> b;
+        --a, --b;
+        int j = log_table[b - a + 1];
+        cout << min(f[a][j], f[b - (1 << j) + 1][j]) << endl;
     }
- 
+
     #ifdef TIMING 
     auto end = chrono::steady_clock::now();
     cout << "\nElapsed time in milliseconds : "
