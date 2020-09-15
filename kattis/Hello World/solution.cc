33c5b876dd8cc935483cfeea986ea29441e6b0a3
Zhiwen Xie <TiwazBiu@gmail.com>
2020-09-15 19:51:40 +0800

    [kattis]first kattis submission

---
+++ kattis/Hello World/solution.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define int __int64_t
+using namespace std;
+
+__int32_t main() {
+    #ifdef TIMING 
+    auto start = chrono::steady_clock::now();
+    #endif
+    #ifdef DEBUG
+    freopen("input.txt", "r", stdin); // redirects standard input
+    freopen("output.txt", "w", stdout); // redirects standard output
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // solve the problem
+    cout << "Hello World!" << endl;
+
+    #ifdef TIMING 
+    auto end = chrono::steady_clock::now();
+    cout << "\nElapsed time in milliseconds : "
+         << chrono::duration_cast<chrono::milliseconds>(end - start).count()
+         << " ms\n";
+    #endif
+    return 0;
+}
