f1ccca921c952fc902d219f460e9495a09589a84
Zhiwen Xie <TiwazBiu@gmail.com>
2020-09-21 16:41:29 +0800

    Coin Combinations II, iterative DP, AC by change the order of n and k dimension of dp array, very surprised, looks like due to  cache

--- cses/3. Dynamic Programming/Coin Combinations II/iterative.cc
+++ cses/3. Dynamic Programming/Coin Combinations II/iterative.cc
@@ -2,7 +2,7 @@
 using namespace std;
 
 const int N = 1e6 + 10, K = 110, MOD = 1e9 + 7;
-int n, k, f[N][K], c[K];
+int n, k, f[K][N], c[K];
 
 int main() {
     #ifdef TIMING 
@@ -16,15 +16,16 @@ int main() {
     // solve the problem
     scanf("%d %d", &k, &n);
     for (int i = 1; i <= k; ++i) scanf("%d", &c[i]);
-    for (int j = 0; j <= k; ++j) f[0][j] = 1;
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= k; ++j) {
-            f[i][j] += f[i][j - 1];
-            if (i >= c[j]) f[i][j] += f[i - c[j]][j];
+    for (int i = 0; i <= k; ++i) f[i][0] = 1;
+
+    for (int i = 1; i <= k; ++i) {
+        for (int j = 1; j <=n; ++j) {
+            f[i][j] += f[i - 1][j];
+            if (j >= c[i]) f[i][j] += f[i][j - c[i]];
             f[i][j] %= MOD;
         }
     }
-    printf("%d\n", f[n][k]);
+    printf("%d\n", f[k][n]);
 
     #ifdef TIMING 
     auto end = chrono::steady_clock::now();
--- cses/3. Dynamic Programming/Coin Combinations II/solution.cc
+++ cses/3. Dynamic Programming/Coin Combinations II/solution.cc
@@ -1,16 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int K = 110, N = 1e6 + 10;
+const int K = 110, N = 1e6 + 10, MOD = 1e9 + 7;
 int k, n, c[K], f[N][K];
 bool solved[N][K];
 
 int solve(int x, int i) {
     if (x < 0 || i < 0) return 0;
     else if (x == 0) return 1;
+    else if (i == 0) return 0;
     else if (solved[x][i]) return f[x][i];
     else {
         f[x][i] = solve(x - c[i], i) + solve(x, i - 1);
+        f[x][i] %= MOD;
         solved[x][i] = true;
         return f[x][i];
     }
@@ -27,11 +29,11 @@ int main() {
 
     // solve the problem
     cin >> k >> n;
-    for (int i = 0; i < k; ++i) cin >> c[i];
+    for (int i = 1; i <= k; ++i) cin >> c[i];
     for (int i = 1; i <= n; ++i)
-        for (int j = 0; j < k; ++j)
+        for (int j = 1; j <= k; ++j)
             solved[i][j] = false;
-    cout << solve(n, k - 1) << endl;
+    cout << solve(n, k) << endl;
 
     #ifdef TIMING 
     auto end = chrono::steady_clock::now();
