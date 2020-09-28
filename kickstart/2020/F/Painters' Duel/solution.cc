63769a700caac0f880a18b9b0bd57e9525ef0592
Zhiwen Xie <TiwazBiu@gmail.com>
2020-09-28 09:41:08 +0800

    kickstart 2020 round F Painters' Duel, still not correct

--- kickstart/2020/F/Painters' Duel/solution.cc
+++ kickstart/2020/F/Painters' Duel/solution.cc
@@ -5,39 +5,80 @@ const int S = 10, INF = 1e9;
 bool painted[S + 1][2 * S];
 int s;
 
-vector<pair<int, int>> next(int r, int p) {
+vector<pair<int, int>> neighbors(const pair<int, int> x) {
+    int r, p;
+    tie(r, p) = x;
     vector<pair<int, int>> v;
-    if (p > 1 && !painted[r][p - 1])
+    if (p - 1 >= 1 && !painted[r][p - 1])
         v.push_back({r, p - 1});
-    if (p < 2 * s - 1 && !painted[r][p + 1])
+    if (p + 1 <= 2 * r - 1 && !painted[r][p + 1])
         v.push_back({r, p + 1});
     if (p % 2 == 0 && r - 1 >= 1 && p - 1 >= 1 && !painted[r - 1][p - 1])
         v.push_back({r - 1, p - 1});
-    if (p % 2 == 1 && r + 1 <= s && p + 1 <= 2 *s - 1 && !painted[r + 1][p + 1])
+    if (p % 2 == 1 && r + 1 <= s && p + 1 <= 2 * (r + 1) - 1 && !painted[r + 1][p + 1])
         v.push_back({r + 1, p + 1});
     return v;
 }
 
-tuple<int, int, int> search(int r1, int p1, int r2, int p2) {
-    vector<pair<int, int>> v1 = next(r1, p1);
-    if (v1.empty()) return {0, r1, p1};
-    int bc1 = 0, br1 = r1, bp1 = p1;
-    for (const auto &pos : v1) {
-        int r3, p3, r4, p4, r5, p5;
-        tie(r3, p3) = pos;
-        painted[r3][p3] = true;
-        int c1, c2;
-        tie(c2, r4, p4) = search(r2, p2, r3, p3);
-        tie(c1, r5, p5) = search(r3, p3, r4, p4);
-        c1 += 1;
-        if (bc1 < c1) {
-            bc1 = c1;
-            br1 = r3;
-            bp1 = p3;
+int search(const pair<int, int> a, const pair<int, int> b) {
+    /* cout << "\na: " << a.first << a.second << endl; */
+    /* cout << "b: " << b.first << b.second << endl; */
+    auto u = neighbors(a), v = neighbors(b);
+    if (u.empty() && v.empty()) {
+        /* cout << "a and b neighbor empty" << endl; */
+        return 0;
+    } else if (u.empty()) {
+        /* cout << "a neighbor empty" << endl; */
+        int worst_score = INF;
+        for (const auto &y : v) {
+            painted[y.first][y.second] = true;
+            int score = search(a, y);
+            if (score < worst_score) {
+                worst_score = score;
+            }
+            painted[y.first][y.second] = false;
         }
-        painted[r3][p3] = false;
+        return -1 + worst_score;
+    } else if (v.empty()) {
+        /* cout << "b neighbor empty" << endl; */
+        int best_score = -INF;
+        for (const auto &x : u) {
+            painted[x.first][x.second] = true;
+            int score = search(x, b);
+            if (score > best_score) {
+                best_score = score;
+            }
+            painted[x.first][x.second] = false;
+        }
+        return 1 + best_score;
     }
-    return {bc1, br1, bp1};
+    int best_score = -INF;
+    for (const auto &x : u) {
+        painted[x.first][x.second] = true;
+        vector<pair<int, int>> v = neighbors(b);
+        if (v.empty()) {
+            int score = 1 + search(x, b);
+            if (score > best_score) {
+                best_score = score;
+            }
+        } else {
+            int worst_score = INF;
+            for (const auto &y : v) {
+                painted[y.first][y.second] = true;
+                int score = search(x, y);
+                if (score < worst_score) {
+                    worst_score = score;
+                }
+                painted[y.first][y.second] = false;
+            }
+            int score = worst_score;
+            if (score > best_score) {
+                best_score = score;
+            }
+        }
+        painted[x.first][x.second] = false;
+    }
+    return best_score;
 }
 
 int main() {
@@ -56,19 +97,17 @@ int main() {
         int ra, pa, rb, pb, c;
         cin >> s >> ra >> pa >> rb >> pb >> c;
         for (int i = 1; i <= s; ++i) {
-            for (int j = 1; j <= 2 * s - 1; ++j) {
+            for (int j = 1; j <= 2 * i - 1; ++j) {
                 painted[i][j] = false;
             }
         }
         for (int i = 0; i < c; ++i) {
-            int a, b;
-            cin >> a >> b;
-            painted[a][b] = true;
+            int r, p;
+            cin >> r >> p;
+            painted[r][p] = true;
         }
-        int ca, cb;
-        tie(ca, ra, pa) = search(ra, pa, rb, pb);
-        tie(cb, rb, pb) = search(rb, pb, ra, pa);
-		cout << "Case #" << t << ": " << ca - cb << endl;
+        pair<int, int> a = {ra, pa}, b = {rb, pb};
+		cout << "Case #" << t << ": " << search(a, b) << endl;
 	}
 
     #ifdef TIMING 
