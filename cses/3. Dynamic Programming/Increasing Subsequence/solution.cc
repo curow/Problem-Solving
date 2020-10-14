42045926df8fdf4273b3767ad7b9521fa6f46f90
Zhiwen Xie <TiwazBiu@gmail.com>
2020-10-14 21:40:00 +0800

    Increasing Subsequence, DP, O(n^2), TLE

---
+++ cses/3. Dynamic Programming/Increasing Subsequence/solution.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 2e5 + 10;
+int a[N], dp[N];
+
+void solve() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; ++i) cin >> a[i];
+    int best = 1;
+    for (int i = 1; i <= n; ++i) {
+        dp[i] = 1;
+        for (int j = 1; j < i; ++j) {
+            if (a[j] < a[i]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+        best = max(best, dp[i]);
+    }
+    cout << best << endl;
+}
+
+int main() {
+#ifdef TIMING
+    auto start = chrono::steady_clock::now();
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+#ifdef DEBUG
+    freopen("input.txt", "r", stdin);  // redirects standard input
+    /* freopen("output.txt", "w", stdout); // redirects standard output */
+#endif
+    // solve the problem
+    solve();
+#ifdef TIMING
+    auto end = chrono::steady_clock::now();
+    cout << "\nElapsed time in milliseconds : "
+         << chrono::duration_cast<chrono::milliseconds>(end - start).count()
+         << " ms\n";
+#endif
+    return 0;
+}
