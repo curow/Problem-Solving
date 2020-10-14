910d934f76bbbedb301e0316ed3bea7a73169a82
Zhiwen Xie <TiwazBiu@gmail.com>
2020-10-14 23:16:56 +0800

    Increasing Subsequence, not sure it's dp, O(nlogn), AC

--- cses/3. Dynamic Programming/Increasing Subsequence/solution.cc
+++ cses/3. Dynamic Programming/Increasing Subsequence/solution.cc
@@ -2,23 +2,21 @@
 using namespace std;
 
 const int N = 2e5 + 10;
-int a[N], dp[N];
+int a[N];
 
 void solve() {
     int n;
     cin >> n;
     for (int i = 1; i <= n; ++i) cin >> a[i];
-    int best = 1;
-    for (int i = 1; i <= n; ++i) {
-        dp[i] = 1;
-        for (int j = 1; j < i; ++j) {
-            if (a[j] < a[i]) {
-                dp[i] = max(dp[i], dp[j] + 1);
-            }
-        }
-        best = max(best, dp[i]);
+    vector<int> dp;
+    dp.reserve(n);
+    dp.push_back(a[1]);
+    for (int i = 2; i <= n; ++i) {
+        auto it = upper_bound(dp.begin(), dp.end(), a[i]);
+        if (it == dp.end() && dp.back() < a[i]) dp.push_back(a[i]);
+        else if (it == dp.begin() || *(it - 1) < a[i]) *it = a[i];
     }
-    cout << best << endl;
+    cout << dp.size() << endl;
 }
 
 int main() {
